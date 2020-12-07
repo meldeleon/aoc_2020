ba0406f5b3008d6956e38aa240772006328e9eb7
meldeleon <melissadeleon@gmail.com>
2020-12-07 09:41:25 -0800

    adding solution 2, day 7

--- day7.js
+++ day7.js
@@ -1,13 +1,16 @@
+const { get } = require("http")
+
 const input = require("fs").readFileSync("day7.txt").toString().split("\n")
 
 //console.log(input)
 
+//format the input, create new array of "container" > "contains" 
 let containerObjectsArray = input.map( sentence => {
     let splitByContain = sentence.split("contain")
     let outerBag = splitByContain[0].replace(/s\s+$/, "")
     let innerBags = splitByContain[1].split(",")
     innerBags = innerBags.map(x=> {
-        let xFixed = x.replace(/s+$/, "").replace(/(s\.)+$/, "").replace(/^\s+/, "")
+        let xFixed = x.replace(/s+$/, "").replace(/(s\.)+$/, "").replace(/^\s+/, "").replace(/\.+$/,"")
         return xFixed
     })
     //console.log(outerBag)
@@ -51,7 +54,6 @@ let toCheckList = ["shiny gold"]
 let checkedList = []
 
 
-
 while (toCheckList.length > 0){
     let currentBag = toCheckList.pop()
     let containerList =  getContainer(currentBag)
@@ -61,11 +63,61 @@ while (toCheckList.length > 0){
         } 
     })
     checkedList.push(currentBag)
-    console.log("TO CHECK:" + toCheckList);
-    console.log("CHECKED:" +checkedList);
-
+    //console.log("TO CHECK:" + toCheckList);
+    //console.log("CHECKED:" +checkedList);
 }
 
 //count all
-let total = checkedList.length - 1
+let setCheckedList = new Set(checkedList)
+let total = setCheckedList.size - 1
 console.log("solution #1:" + total)
+
+
+//solution 2
+function getContained(container){
+    let containedBagArray = []
+    containerObjectsArray.forEach( x => {
+        if (x.name.includes(container)){
+            containedBagArray = containedBagArray.concat(x.contains)
+        }
+        else {}
+    })
+    return containedBagArray
+}
+
+let toCheckList2 = ["1 shiny gold bag"]
+let checkedList2 = []
+let totalBags = 0
+
+while (toCheckList2.length > 0) {
+    let currentBag = toCheckList2.pop()
+    console.log("current bag:" + currentBag)
+    let checkModifier = currentBag.match(/^[0-9]+/)
+    bagName = currentBag.match(/(?<=\s)(.*)$/)[0]
+    console.log(bagName)
+    console.log("check modifier: " + checkModifier)
+    if (!isNaN(checkModifier)){
+        let containerModifier = checkModifier;
+        let containedList = getContained(bagName);
+        console.log("contained list:" + containedList)
+        containedList.forEach( x =>{
+            let modifier = parseInt(x[0])
+            if (isNaN(modifier)){
+                return
+            }
+            let modifierXModifier = containerModifier * modifier
+            let modifiedCheckBag = x.replace(modifier, modifierXModifier) 
+                console.log("modified check bag: " + modifiedCheckBag)
+                totalBags += modifierXModifier
+                console.log("adding "+ modifierXModifier +" to running total")
+            if(!checkedList.includes(x)){   
+                toCheckList2.push(modifiedCheckBag)
+                  
+            }
+        })
+        checkedList2.push(currentBag)
+    }
+    
+}
+    
+console.log("solution #2:" + totalBags)
