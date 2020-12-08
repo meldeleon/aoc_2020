3ce22489b80a48770493cab6c6c4785faa4e1f27
meldeleon <melissadeleon@gmail.com>
2020-12-08 09:35:08 -0800

    refactoring solution for day 1

--- day8.js
+++ day8.js
@@ -1,47 +1,34 @@
 const input = require("fs").readFileSync("day8.txt").toString().split("\n")
+const inputMatrix = input.flatMap( x => { 
+    let array = new Array(x.split(" "))
+    array.forEach( x => {
+        x.splice(1,1,parseInt(x[1]))
+    })
+    return array
+})
 
-
-let executed = new Array(input.length)
+console.log(inputMatrix)
+let executed = new Array(inputMatrix.length)
 let accumulator = 0
-let loopNumber = 0
+let counter = 0
 
-function executeOrder (commandType, commandIndex, argument) {
-    loopNumber++
-    console.log("===== LOOP START NUMBER " + loopNumber + "=====")
-    let currentCommand = input[commandIndex]
-    let nextIndex = commandIndex + 1
-    console.log("next index: " + nextIndex)
-    let nextCommand = input[nextIndex]
-    let nextType = nextCommand.substring(0,3)
-    let nextArgument = nextCommand.match(/(?<=\s).*$/)[0]
+
+while(executed[counter] === undefined){
     console.table(executed)
-    if (executed[commandIndex] !== currentCommand ){       
-        console.log("command index: " + commandIndex)
-        console.log("argument: "+ argument)
-        console.log("next parameters: " + nextType + nextArgument)
-        console.log("executed " + commandType + " " + argument + " at " + commandIndex)
-        executed.splice(commandIndex, 1, currentCommand)
-        if (commandType === "acc") {
-            accumulator = accumulator + parseInt(argument)
-            console.log("accumulator is " + accumulator)
-            executeOrder(nextType, nextIndex, nextArgument)
-        }
-        else if (commandType === "jmp") {
-            let jumpedIndex = commandIndex + parseInt(argument)
-            console.log("jumped index: " + jumpedIndex)
-            let jumpedCommand = input[jumpedIndex]
-            let jumpedType = jumpedCommand.substring(0,3)
-            let jumpedArgument = jumpedCommand.match(/(?<=\s).*$/)[0]
-            executeOrder(jumpedType, jumpedIndex, jumpedArgument)
-        } else {
-            //do nothing
-            executeOrder(nextType, nextIndex, nextArgument)
-        }
-    }else {
-        console.log("looped once")
-        console.log("final accumulator: " + parseInt(accumulator))
-        return;
+    let currentCommand = inputMatrix[counter]
+    console.log(currentCommand) 
+    let command, argument
+    [command, argument] = currentCommand
+    console.log(command)
+    if (command === 'acc') {
+        accumulator = accumulator + argument
+        executed.splice(counter, 1, inputMatrix[counter])
+        counter ++
+    } else if (command === "jmp") {
+        counter += argument
+    } else {
+        //do nothing
+        counter ++
     }
 }
-
-executeOrder("acc",0,3)
+console.log("final accumulator: " + accumulator)
