5d2e380137436f201dbe2a05176ce70770863fe0
meldeleon <melissadeleon@gmail.com>
2020-12-08 12:46:58 -0800

    solution 2 working for small input, but not for  big input

--- day8.js
+++ day8.js
@@ -1,34 +1,42 @@
-const input = require("fs").readFileSync("day8.txt").toString().split("\n")
-const inputMatrix = input.flatMap( x => { 
-    let array = new Array(x.split(" "))
-    array.forEach( x => {
-        x.splice(1,1,parseInt(x[1]))
-    })
-    return array
-})
+const input = require("fs").readFileSync("day8.txt").toString().split("\n");
+const inputMatrix = input.flatMap((x) => {
+  let array = new Array(x.split(" "));
+  array.forEach((x) => {
+    x.splice(1, 1, parseInt(x[1]));
+  });
+  return array;
+});
 
-console.log(inputMatrix)
-let executed = new Array(inputMatrix.length)
-let accumulator = 0
-let counter = 0
+console.log(inputMatrix);
+let executed = new Array(inputMatrix.length);
+let accumulator = 0;
+let counter = 0;
 
+//solution 1
 
-while(executed[counter] === undefined){
-    console.table(executed)
-    let currentCommand = inputMatrix[counter]
-    console.log(currentCommand) 
-    let command, argument
-    [command, argument] = currentCommand
-    console.log(command)
-    if (command === 'acc') {
-        accumulator = accumulator + argument
-        executed.splice(counter, 1, inputMatrix[counter])
-        counter ++
+while (executed[counter] === undefined) {
+  if (counter >= inputMatrix.length) {
+    console.log("PROGRAM TERMINATED// final accumulator: " + accumulator);
+    return;
+  } else {
+    console.table(executed);
+    let currentCommand = inputMatrix[counter];
+    console.log(currentCommand);
+    let command, argument;
+    [command, argument] = currentCommand;
+    console.log(command);
+    if (command === "acc") {
+      accumulator = accumulator + argument;
+      executed.splice(counter, 1, inputMatrix[counter]);
+      counter++;
     } else if (command === "jmp") {
-        counter += argument
+      counter += argument;
     } else {
-        //do nothing
-        counter ++
+      //do nothing
+      counter++;
     }
+  }
 }
-console.log("final accumulator: " + accumulator)
+console.log(
+  "TERMINATED BEFORE INFINITE LOOP // final accumulator: " + accumulator
+);
---
+++ day8_pt2.js
@@ -0,0 +1,108 @@
+const input = require("fs").readFileSync("day8.txt").toString().split("\n");
+const inputMatrix = input.flatMap((x) => {
+  let array = new Array(x.split(" "));
+  array.forEach((x) => {
+    x.splice(1, 1, parseInt(x[1]));
+  });
+  return array;
+});
+
+let executed = new Array(inputMatrix.length);
+let accumulator = 0;
+let counter = 0;
+
+runLoop(inputMatrix, counter, accumulator);
+
+function runLoop(inputMatrix, counter, accumulator) {
+  if (counter >= inputMatrix.length) {
+    console.log(
+      "PROGRAM TERMINATED NATURALLY// final accumulator: " + accumulator
+    );
+    return;
+  } else {
+    while (executed[counter] === undefined) {
+      let currentCommand = inputMatrix[counter];
+      let command, argument;
+      [command, argument] = currentCommand;
+      console.log("==== ORIGINAL LOOP Check =====");
+      console.log("current iteration: " + currentCommand);
+      console.log("current command: " + command);
+      if (command === "acc") {
+        accumulator = accumulator + argument;
+        executed.splice(counter, 1, inputMatrix[counter]);
+        counter++;
+      } else if (command === "jmp") {
+        if (
+          runAlternate(inputMatrix, counter, executed, accumulator) === true
+        ) {
+          return;
+        } else {
+          counter += argument;
+        }
+      } else {
+        if (
+          runAlternate(inputMatrix, counter, executed, accumulator) === true
+        ) {
+          return;
+        } else {
+          counter++;
+        }
+      }
+    }
+  }
+  console.log("loop ended for original condition");
+  console.log("final accumulator: " + accumulator);
+}
+
+function runAlternate(inputMatrix, counter, executed, accumulator) {
+  let alternateCommand = null;
+  let currentCommand = inputMatrix[counter];
+  console.log("running alternate scenario for: " + currentCommand);
+  let command, argument;
+  [command, argument] = currentCommand;
+  if (command === "jmp") {
+    alternateCommand = "nop";
+  } else if (command === "nop") {
+    alternateCommand = "jmp";
+  } else {
+    alternateCommand = "acc";
+  }
+  let alternateInputMatrix = [...inputMatrix];
+  alternateInputMatrix.splice(counter, 1, [alternateCommand, argument]);
+  console.log("alternate index: " + counter);
+  //run alternate
+  while (executed[counter] === undefined) {
+    if (counter >= alternateInputMatrix.length) {
+      console.log(
+        "ALTERNATE PROGRAM TERMINATED NATURALLY// final accumulator: " +
+          accumulator
+      );
+      return true;
+    } else {
+      console.table(executed);
+      let currentCommand = alternateInputMatrix[counter];
+      console.log(currentCommand);
+      let command, argument;
+      [command, argument] = currentCommand;
+      console.log(command);
+      if (command === "acc") {
+        accumulator = accumulator + argument;
+        executed.splice(counter, 1, alternateInputMatrix[counter]);
+        counter++;
+      } else if (command === "jmp") {
+        if (argument > 0) {
+          counter += argument;
+        } else {
+          counter++;
+        }
+      } else {
+        //do nothing
+        counter++;
+      }
+    }
+  }
+  console.log(
+    "TERMINATED BEFORE INFINITE LOOP // final accumulator: " + accumulator
+  );
+  return false;
+}
