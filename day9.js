f331520ddc801a83af179993e85b17a40f684237
meldeleon <melissadeleon@gmail.com>
2020-12-08 21:53:53 -0800

    day 9 solution 1 attempt

--- day9.js
+++ day9.js
@@ -5,14 +5,14 @@ console.log(input);
 for (i = preamble; i < input.length; i++) {
   console.log("checking " + input[i]);
   for (j = i; j > i - preamble; j--) {
-    let difference = input[i] - input[j - 1];
+    let difference = parseInt(input[i] - input[j - 1]);
     console.log(difference);
 
     let slice = input.slice(i - preamble, i);
     console.log("slice: " + slice);
     console.log(slice.includes(difference));
 
-    if (slice.includes(difference) === true) {
+    if (slice.includes(difference)) {
       console.log(input[i] + " is good");
     }
   }
