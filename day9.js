7a2bacaeed6da76386f3eb41cd1d598007831c4c
meldeleon <melissadeleon@gmail.com>
2020-12-08 21:52:11 -0800

    day 9 solution 1 attempt

--- day9.js
+++ day9.js
@@ -1,1 +1,19 @@
 const input = require("fs").readFileSync("day9.txt").toString().split("\n");
+const preamble = 5;
+console.log(input);
+
+for (i = preamble; i < input.length; i++) {
+  console.log("checking " + input[i]);
+  for (j = i; j > i - preamble; j--) {
+    let difference = input[i] - input[j - 1];
+    console.log(difference);
+
+    let slice = input.slice(i - preamble, i);
+    console.log("slice: " + slice);
+    console.log(slice.includes(difference));
+
+    if (slice.includes(difference) === true) {
+      console.log(input[i] + " is good");
+    }
+  }
+}
