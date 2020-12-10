8aeb51c4fc3a280d9227b3174ce679ecf8e95aea
meldeleon <melissadeleon@gmail.com>
2020-12-09 21:29:44 -0800

    day 10 part 1 solution

--- day10.js
+++ day10.js
@@ -1,1 +1,45 @@
-const imported = require("fs").readFileSync("day09.txt").toString().split("\n")
+const imported = require("fs")
+  .readFileSync("day10.txt")
+  .toString()
+  .split("\r\n")
+
+let input = imported.map((x) => {
+  return parseInt(x)
+})
+
+//start with plug
+input.push(0)
+
+input = input.sort(function (a, b) {
+  return a - b
+})
+
+let differenceArray = []
+
+for (i = 0; i < input.length; i++) {
+  let difference = input[i + 1] - input[i]
+  if (difference <= 3) {
+    differenceArray.push(difference)
+  }
+}
+
+//final adapter
+differenceArray.push(3)
+
+differenceArray = differenceArray.sort(function (a, b) {
+  return a - b
+})
+
+//console.log(differenceArray)
+let count1 = 0
+let count3 = 0
+
+for (i = 0; i < differenceArray.length; i++) {
+  if (differenceArray[i] === 1) {
+    count1++
+  } else if (differenceArray[i] === 3) {
+    count3++
+  }
+}
+let answer = count1 * count3
+console.log("solution 1 is: " + answer)
